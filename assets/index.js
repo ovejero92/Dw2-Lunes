c3dc7320c6c09398d40ee4f42e850d06eda01a0c
Gustavo <gustavo.ovejero@educacion.local>
2025-04-15 01:59:02 -0300

    actualizacion de clase2

--- assets/index.js
+++ assets/index.js
@@ -1,4 +1,33 @@
-alert("Alerta desde js");
-
+//alert("Alerta desde js");
+//asdkjaslkdjsalkdjalkjdaslkdj slakjdlakj salj asjl 
+/*
+asldkaslkdjaslkd
+asdjaskldjalkdasjda
+sdjasdasldkasjda
+*/
 console.log("Hola desde index.js");
 
+const nombreInstructor = "Ezequiel"
+const apellidoInstructor = "Mondino"
+
+const nombreCompleto = nombreInstructor + " " + apellidoInstructor
+
+const nombreCompletoMejorado = `${nombreInstructor} ${apellidoInstructor}`
+
+let edad = 1992;
+
+
+let FN = prompt("Indicame tu fecha de nacimiento:")
+
+let edadUsuario = 2025 - FN
+
+alert("tu edad es: " + edadUsuario)
+
+
+edad = 30
+
+
+
+
+
+console.log("mi edad es:" + edad)
